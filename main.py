18532e25b261378f10583822659e8b48e17baa8f
kyleaddis <kyle.a.addis@gmail.com>
2024-10-03 11:27:05 +0530

    added player shape

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
+from player import Player
 from constants import *
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -11,7 +15,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
